bf5f0fe4030b6a5627bb508ad5caff36fd0bde9a
yesmine96 <yesmine.ghorbel96@gmail.com>
2025-10-29 09:05:47 +0100

    chore(lint): add ESLint rules and Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import prettier from "eslint-config-prettier";
 
 export default defineConfig([
   globalIgnores(["dist"]),
@@ -14,10 +16,27 @@ export default defineConfig([
       tseslint.configs.recommendedTypeChecked,
       reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
+      prettier,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      "prettier/prettier": "warn",
+      "prefer-const": "warn",
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      eqeqeq: ["warn", "smart"],
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+      "react/jsx-boolean-value": ["warn", "never"],
+      "react-hooks/exhaustive-deps": "warn",
     },
   },
 ]);
