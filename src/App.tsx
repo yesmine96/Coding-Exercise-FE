bf271f345697c5d5603e6fb46e0da692acae22bf
yesmine96 <yesmine.ghorbel96@gmail.com>
2025-10-30 18:22:12 +0100

    feat: centralize app colors and style in one global CSS file

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import CalendarOverviewPage from "./pages/CalendarOverviewPage";
 import AddEventPage from "./pages/AddEventPage";
+import "../src/styles/globals.css";
 
 function App() {
   return (
