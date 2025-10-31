8d776438bf5d489f056d48fa651b44a855d95084
yesmine96 <yesmine.ghorbel96@gmail.com>
2025-10-31 17:31:25 +0100

    feat add header component

---
+++ src/features/calendar/CalendarHeader.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface CalendarHeaderProps {
+  monthName: string;
+  year: number;
+}
+
+const CalendarHeader: React.FC<CalendarHeaderProps> = ({ monthName, year }) => {
+  return (
+    <div className="text-center py-8 lg:py-14">
+      <h2 className="text-3xl lg:text-5xl font-bold">
+        {monthName} {year}
+      </h2>
+    </div>
+  );
+};
+
+export default CalendarHeader;
--- src/pages/CalendarOverviewPage.tsx
+++ src/pages/CalendarOverviewPage.tsx
@@ -1,11 +1,15 @@
 import Calendar from "../features/calendar/Calendar";
+import CalendarHeader from "../features/calendar/CalendarHeader";
 const today = new Date();
 const year = today.getFullYear();
 const month = today.getMonth();
+const monthName = today.toLocaleString("default", { month: "long" });
 
 export default function CalendarOverviewPage() {
   return (
-    <div className="px-16">
+    <div className="px-2 lg:px-16 pt-28 ">
+      <CalendarHeader monthName={monthName} year={year} />
+
       <Calendar year={year} month={month} />
     </div>
   );
