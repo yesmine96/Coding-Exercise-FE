38a62923aa939d1caa8eba4b16f1e76e904e20dc
yesmine96 <yesmine.ghorbel96@gmail.com>
2025-11-06 15:57:03 +0100

    refactor: change navbar links to NavLink for active styling

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,10 +1,10 @@
 import React, { useCallback, useEffect, useState } from "react";
-import clsx from "clsx";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Logo from "../ui/Logo";
-import type { NavLink } from "../../types/nav";
+import type { NavLinkType } from "../../types/nav";
+import { cn } from "../../utils/cn";
 
-const navLinks: NavLink[] = [
+const navLinks: NavLinkType[] = [
   { label: "Calendar", href: "/" },
   { label: "Add event", href: "/calendar/add" },
 ];
@@ -37,27 +37,32 @@ const Navbar: React.FC = () => {
 
   return (
     <header
-      className={clsx(
+      className={cn(
         "bg-secondary text-white shadow-md z-50 fixed w-full",
         isOpen ? "h-screen" : "h-auto"
       )}
     >
       <nav className=" w-full px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-8">
-          <Link to="/" className="flex items-center">
+          <NavLink to="/" className="flex items-center">
             <Logo className="xl:w-72 w-54" />
-          </Link>
+          </NavLink>
 
           {/* Desktop Menu */}
           <div className="hidden md:flex items-center space-x-10 text-xl font-semibold">
             {navLinks.map((link) => (
-              <Link
+              <NavLink
                 key={link.href}
                 to={link.href}
-                className="hover:text-primary transition-colors duration-200"
+                className={({ isActive }) =>
+                  cn(
+                    "hover:text-primary text-white transition-colors duration-200",
+                    isActive && "text-primary font-bold"
+                  )
+                }
               >
                 {link.label}
-              </Link>
+              </NavLink>
             ))}
           </div>
 
@@ -69,21 +74,21 @@ const Navbar: React.FC = () => {
             aria-expanded={isOpen}
           >
             <span
-              className={clsx(
-                "block bg-primary w-full h-0.5 transition-transform duration-300",
+              className={cn(
+                "block bg-primary w-full h-0.5 text-white transition-transform duration-300",
                 { "rotate-45 absolute top-1/2 -translate-y-1/2": isOpen }
               )}
             />
 
             <span
-              className={clsx(
+              className={cn(
                 "block bg-primary w-full h-0.5 transition-all duration-300",
                 { hidden: isOpen }
               )}
             />
 
             <span
-              className={clsx(
+              className={cn(
                 "block bg-primary w-full h-0.5 transition-transform duration-300",
                 { "-rotate-45 absolute top-1/2 -translate-y-1/2": isOpen }
               )}
@@ -94,18 +99,23 @@ const Navbar: React.FC = () => {
 
       {/* Mobile Menu */}
       <div
-        className={clsx("md:hidden bg-secondary", isOpen ? "h-full" : "hidden")}
+        className={cn("md:hidden bg-secondary", isOpen ? "h-full" : "hidden")}
       >
         <div className="px-4 pt-2 pb-4 space-y-3 text-xl font-semibold">
           {navLinks.map((link) => (
-            <Link
+            <NavLink
               key={link.href}
               to={link.href}
               onClick={closeMenu}
-              className="block text-gray-200 hover:text-gray-400 border-t border-gray-800 py-4"
+              className={({ isActive }) =>
+                cn(
+                  "block text-gray-200 hover:text-gray-400 border-t border-gray-800 py-4",
+                  isActive && "text-primary font-bold"
+                )
+              }
             >
               {link.label}
-            </Link>
+            </NavLink>
           ))}
         </div>
       </div>
